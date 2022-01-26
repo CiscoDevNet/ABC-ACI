5d7e792bc2c73015b596695bfdcaa8d94cdb92a3
Juulia Santala <jusantal@cisco.com>
2022-01-26 23:54:46 +0200

    initing the epg for hot, medium, and mild

---
+++ python_requests/hot/epg.py
@@ -0,0 +1,39 @@
+'''Python Requests Excercise - Creating a new endpoint group
+
+This script, once completed, allows the user to create their own bridge
+endpoint group under a selected application profile and linked to a selected
+bridge domain. Note that authentication token is needed for the API calls, for 
+which the script imports the get_token function from authentication module 
+(authentication.py).
+
+- SPICE LEVEL: HOT
+- TASK: Replace the # TODO comments with correct code based on the lab guide
+  instructions
+
+'''
+
+# Import required libraries
+import json
+import requests
+import urllib3
+
+# Import the function get_token from authentication.py
+from authentication import get_token
+
+# Disable unverified HTTPS request warnings
+urllib3.disable_warnings()
+
+def get_epgs(cookie):
+    '''Prints out all the ACI endpoint groups'''
+
+    # TODO: Code to retrieve and print out all the EPGs
+
+def create_epg(cookie, tenant_name, bd_name, ap_name, epg_name):
+    '''Creates a new ACI endpoint group'''
+
+    # TODO: Code to create a new EPG
+
+# The following if statement is True when this file is executed directly.
+if __name__ == "__main__":
+    token_cookie = get_token()
+    get_epgs(token_cookie)
---
+++ python_requests/mild/epg.py
@@ -0,0 +1,56 @@
+'''Python Requests Excercise - Creating a new endpoint group
+
+This script, once completed, allows the user to create their own bridge
+endpoint group under a selected application profile and linked to a selected
+bridge domain. Note that authentication token is needed for the API calls, for 
+which the script imports the get_token function from authentication module 
+(authentication.py).
+
+- SPICE LEVEL: MILD
+- TASK: Replace the <TODO> with correct code based on the lab guide
+  instructions
+
+'''
+
+# Import required libraries
+import json
+import requests
+import urllib3
+
+# Import the function get_token from authentication.py
+from authentication import get_token
+
+# Disable unverified HTTPS request warnings
+urllib3.disable_warnings()
+
+def get_epgs(cookie):
+    '''Prints out all the ACI endpoint groups'''
+
+    url = <TODO>
+
+    response = requests.request(<TODO>)
+
+    # Print the status code with the URL for information purpose
+    print(f"<Status code {<TODO>} for {url}>")
+
+    #response_dict = <TODO>
+    
+    # Print out the EPGs
+    #<TODO>
+
+def create_epg(cookie, tenant_name, bd_name, ap_name, epg_name):
+    '''Creates a new ACI endpoint group'''
+
+    #url = f"https://apic/api/mo/uni/tn-{<TODO>}/ap-{<TODO>}.json"
+
+    #payload = <TODO>
+
+    #response = requests.request(<TODO>)
+
+    # Print the status code with the URL for information purpose
+    #print(f"<Status code {<TODO>} for creating {epg_name} at {url}>")
+
+# The following if statement is True when this file is executed directly.
+if __name__ == "__main__":
+    token_cookie = get_token()
+    get_epgs(token_cookie)
