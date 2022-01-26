8b27eae0964374e25d495a0404f02b1d811c8567
Juulia Santala <jusantal@cisco.com>
2022-01-26 23:39:39 +0200

    initial solution epg file

---
+++ python_requests/solutions/epg.py
@@ -0,0 +1,72 @@
+'''Python Requests Excercise - Creating a new endpoint group
+
+SOLUTION FILE
+
+This script, once completed, allows the user to create their own bridge
+endpoint group under a selected application profile and linked to a selected
+bridge domain. Note that authentication token is needed for the API calls, for
+which the script imports the get_token function from authentication module
+(authentication.py).
+'''
+
+# Import required libraries
+import json
+import requests
+import urllib3
+
+# Import the function get_token from authentication.py
+from authentication import get_token
+
+# Disable unverified HTTPS request warnings
+urllib3.disable_warnings()
+
+def get_epgs(cookie):
+    '''Prints out all the ACI endpoint groups'''
+
+    url = "https://apic/api/class/fvAEPg.json"
+
+    response = requests.request("GET", url, cookies=cookie, verify=False)
+    print(f"<Status code {response.status_code} for {url}>")
+
+    epg_list = response.json()["imdata"]
+    print("List of the ACI endpoint groups:")
+    for epg in epg_list:
+        print(epg["fvAEPg"]["attributes"]["name"])
+
+def create_epg(cookie, tenant_name, bd_name, ap_name, epg_name):
+    '''Creates a new ACI endpoint group'''
+
+    url = f"https://apic/api/mo/uni/tn-{tenant_name}/ap-{ap_name}.json"
+
+    payload = json.dumps({
+        "fvAEPg": {
+            "attributes": {
+                "name": epg_name,
+                "status": "created"
+            },
+            "children": [
+                {
+                    "fvRsBd": {
+                        "attributes": {
+                            "tnFvBDName":bd_name,
+                            "status":"created,modified"
+                        }
+                    }
+                }
+            ]
+        }
+    })
+
+    response = requests.request("POST", url, cookies=cookie, data=payload, verify=False)
+    print(f"<Status code {response.status_code} for creating {epg_name} at {url}>")
+
+if __name__ == "__main__":
+    TENANT_NAME = "Sales"
+    BD_NAME = "DB_BD"
+    AP_NAME = "eCommerce"
+    EPG_NAME = "DB_EPG"
+
+    token_cookie = get_token()
+    get_epgs(token_cookie)
+    create_epg(token_cookie, TENANT_NAME, BD_NAME, AP_NAME, EPG_NAME)
+    get_epgs(token_cookie)
