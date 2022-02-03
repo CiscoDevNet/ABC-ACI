64773a77283d858afd41ff186f6925088c622d81
Juulia Santala <jusantal@cisco.com>
2022-02-04 01:01:00 +0200

    adding print_tenants mild, medium, hot + updating solutions file

--- aci_cobra_exercises/hot/print_tenants.py
+++ aci_cobra_exercises/hot/print_tenants.py
@@ -5,49 +5,47 @@ SOLUTION FILE
 This script, once completed, allows the user to print out tenant names.
 In the script you get to include both querying a class and an MO with
 DN.
+
+- SPICE LEVEL: HOT
+- TASK: Replace the # <TODO> comments with correct code based on the lab guide
+  instructions
+
 '''
 
 # Import required libraries
-from cobra.mit.access import MoDirectory
-from cobra.mit.session import LoginSession
-import urllib3
+# <TODO>
 
 # Disable unverified HTTPS request warnings
-urllib3.disable_warnings()
+# <TODO>
 
 def print_tenants(url, username, password):
     '''Prints out the tenant names'''
 
     # Login into a session with APIC
-    session = LoginSession(url, username, password)
-    mo_dir = MoDirectory(session)
-    mo_dir.login()
+    # <TODO>: create a LoginSession
+    # <TODO>: create a MoDirectory
+    # <TODO>: login
 
     # Query for tenants using class
-    tenants_class = mo_dir.lookupByClass("fvTenant")
+    # <TODO>
 
     print("\nPrinting information from the class request")
-    # print(tenants_class)
-    for tenant in tenants_class:
-        # print(tenant)
-        # print(dir(tenant))
-        print(tenant.name)
+    # <TODO>
 
     # Query for specific tenant using DN
-    tenant_dn = mo_dir.lookupByDn("uni/tn-Sales")
+    # <TODO>
 
     print("\nPrinting information from the MO request")
-    # print(tenant_dn)
-    print(tenant_dn.name)
+    # <TODO>
 
-    mo_dir.logout()
+    # <TODO> logout
 
 # The following if statement is True when this file is executed directly.
 if __name__ == "__main__":
-    
+
     # URL and credentials information:
-    URL = "https://apic"
-    USERNAME = "admin"
-    PASSWORD = "C1sco12345"
+    # <TODO>: URL
+    # <TODO>: USERNAME
+    # <TODO>: PASSWORD
 
     print_tenants(URL, USERNAME,PASSWORD)
--- aci_cobra_exercises/mild/print_tenants.py
+++ aci_cobra_exercises/mild/print_tenants.py
@@ -5,49 +5,51 @@ SOLUTION FILE
 This script, once completed, allows the user to print out tenant names.
 In the script you get to include both querying a class and an MO with
 DN.
+
+- SPICE LEVEL: MILD
+- TASK: Replace the <TODO> with correct code based on the lab guide
+  instructions
+
 '''
 
 # Import required libraries
-from cobra.mit.access import MoDirectory
-from cobra.mit.session import LoginSession
-import urllib3
+from cobra.mit.access import <TODO>
+from cobra.mit.session import <TODO>
+#import <TODO>
 
 # Disable unverified HTTPS request warnings
-urllib3.disable_warnings()
+# <TODO>
 
 def print_tenants(url, username, password):
     '''Prints out the tenant names'''
 
     # Login into a session with APIC
-    session = LoginSession(url, username, password)
-    mo_dir = MoDirectory(session)
-    mo_dir.login()
+    session = <TODO>(url, username, password)
+    mo_dir = <TODO>(session)
+    mo_dir.<TODO>
 
     # Query for tenants using class
-    tenants_class = mo_dir.lookupByClass("fvTenant")
+    tenants_class = mo_dir.lookupByClass(<TODO>)
 
     print("\nPrinting information from the class request")
-    # print(tenants_class)
-    for tenant in tenants_class:
-        # print(tenant)
-        # print(dir(tenant))
-        print(tenant.name)
+    print(<TODO>)
+    # for tenant in tenants_class:
+        #<todo>
 
     # Query for specific tenant using DN
-    tenant_dn = mo_dir.lookupByDn("uni/tn-Sales")
+    # tenant_dn = mo_dir.<TODO>('uni/tn-Sales')
 
     print("\nPrinting information from the MO request")
     # print(tenant_dn)
-    print(tenant_dn.name)
 
-    mo_dir.logout()
+    mo_dir.<TODO>()
 
 # The following if statement is True when this file is executed directly.
 if __name__ == "__main__":
-    
+
     # URL and credentials information:
-    URL = "https://apic"
-    USERNAME = "admin"
-    PASSWORD = "C1sco12345"
+    URL = "https://<TODO>"
+    USERNAME = "<TODO>"
+    PASSWORD = "<TODO>"
 
     print_tenants(URL, USERNAME,PASSWORD)
